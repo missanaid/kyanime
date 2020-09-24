30ed3236f50bddc2bf3ed682f97fe9b29c559439
missanaid <ariadna.marull@gmail.com>
2020-09-24 18:13:47 -0300

    primer commit

---
+++ js/index.js
@@ -0,0 +1,284 @@
+//Anime
+$("#boruto-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#boruto-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#boku-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#boku-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#naruto-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#naruto-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#bleach-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#bleach-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#zodiaco-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#zodiaco-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#death-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#death-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#conan-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#conan-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#dbz-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#dbz-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#evangelion-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#evangelion-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#dbs-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#dbs-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#fma-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#fma-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#hunter-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#hunter-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#kimetsu-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#kimetsu-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#onepiece-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#onepiece-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#onepunch-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#onepunch-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#pokemon-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#pokemon-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#sailor-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#sailor-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#shingeki-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#shingeki-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#noragami-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#noragami-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#seven-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#seven-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#jojo-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#jojo-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#gintama-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#gintama-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#tokyo-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#tokyo-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#sao-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#sao-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#rezero-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#rezero-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#black-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#black-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#blends-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#blends-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#sora-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#sora-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#game-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#game-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#fairy-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#fairy-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#darling-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#darling-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+$("#god-card").click(function () {
+  $("#contenedor-cards").hide();
+  $(".carousel").hide();
+  $("#god-desc").show();
+  $("#btnatras").show();
+  $(".ocul-foot").hide();
+});
+
+// Botón Atrás
+$("#btnatras").click(function () {
+  $("#contenedor-cards").show();
+  $(".desc").hide();
+  $("#btnatras").hide();
+  $(".carousel").show();
+  $(".ocul-foot").show();
+});
+
+//Botón Arriba
+$(window).scroll(function () {
+  var height = $(window).scrollTop();
+  if (height > 100) {
+    $("#arriba").fadeIn();
+  } else {
+    $("#arriba").fadeOut();
+  }
+});
+
+$("#arriba").click(function (event) {
+  event.preventDefault();
+  $("html, body").animate(
+    { scrollTop: $("#contenedor-cards").offset().top },
+    "slow"
+  );
+  return false;
+});
---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(".news-ticker").marquee({
+  duration: 15000,
+  delayBeforeStart: -10,
+  direction: "left",
+  duplicated: true,
+});
